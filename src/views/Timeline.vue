61a54e4d9b986f752ef2c399477c9ae6e67ce3da
nicolas <nicolas.ducteil@omnilive.fr>
2019-08-01 17:53:02 +0200

    affichage dynamique des tags sur timeline

--- src/views/Timeline.vue
+++ src/views/Timeline.vue
@@ -3,7 +3,7 @@
     <div style="display: flex; flex-direction: column; justify-content: flex-end; align-items: center; height: 100%;">
       <div style="overflow-x: scroll; width: 80%; position: relative; height: 15%">
         <div class="timeline">
-          <div v-for="tag in tagsStyle" :key="tag.id" :style="tag.style"></div>
+          <div v-for="tag in tagsStyle" :key="tag.id" :style="tag"></div>
         </div>
       </div>
     </div>
@@ -18,56 +18,9 @@ export default {
   data() {
     return {
       roomInfos: [],
-      tags: [
-        {
-          backgroundColor: 'red',
-          top: '-30px',
-          left: '3%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-        {
-          backgroundColor: 'blue',
-          top: '-30px',
-          left: '15%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-        {
-          backgroundColor: 'green',
-          top: '-30px',
-          left: '25%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-        {
-          backgroundColor: 'yellow',
-          top: '-30px',
-          left: '28%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-        {
-          backgroundColor: 'purple',
-          top: '-30px',
-          left: '56%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-        {
-          backgroundColor: 'orange',
-          top: '-30px',
-          left: '89%',
-          height: '20px',
-          width: '15px',
-          position: 'absolute',
-        },
-      ],
+      classDuration: '',
+      tags: [],
+      // tagsPosition: [],
       // avatar: '../public/img/avatar/slash.jpg',
     };
   },
@@ -78,17 +31,26 @@ export default {
   },
 
   computed: {
+    // calcCoordinates(time, startClass) {
+    //   const tagSecondes = time - startClass;
+    //   console.log('tag sec ', tagSecondes);
+    //   const tagPixPosition = tagSecondes * (1800 / this.classDuration);
+    //   console.log('tag PixPosition ', tagPixPosition);
+    //   const tagTimelinePosition = tagPixPosition / (1800 / 100);
+    //   console.log(tagTimelinePosition);
+    //   return tagTimelinePosition;
+    //   // console.log('position', tagTimelinePosition);
+    //   // console.log(tagTimelinePosition);
+    // },
     tagsStyle() {
       return this.tags.map(tag => ({
         ...tag,
-        style: {
-          backgroundColor: tag.backgroundColor,
-          top: tag.top,
-          left: tag.left,
-          height: tag.height,
-          width: tag.width,
-          position: tag.position,
-        },
+        backgroundColor: tag.color,
+        top: '-30px',
+        left: `${(((tag.time - this.roomInfos[0].startClass) * (1800 / this.classDuration)) / 18)}%`, // calcul de la position du tag en pourcentage par rapport à son timestamp : (tag timestamp- timestamp du début du cours) * (width de la div de la timeline (ici définie à 1800px) / durée du cours en secondes). Et ce résultat est divisé par la width de la div / 100 pour obtenir un nombre correspondant à un pourcentage de distance par rapport au début de la div donc du cours
+        height: '20px',
+        width: '15px',
+        position: 'absolute',
       }));
     },
   },
@@ -102,7 +64,33 @@ export default {
           if (err) {
             console.log(err);
           } else if (data.status === 200) {
-            this.roomInfos.push(data);
+            console.log('data', data);
+            const roomData = data;
+            console.log('roomData', roomData);
+            const {
+              startClass, endClass, authorID, authorFirstname, authorLastname, authorUsername, title,
+            } = roomData.content1[0];
+            console.log(startClass);
+            console.log(endClass);
+            this.roomInfos.push({
+              roomID,
+              authorID,
+              authorFirstname,
+              authorLastname,
+              authorUsername,
+              title,
+              startClass,
+              endClass,
+            });
+            const classDuration = endClass - startClass;
+            this.classDuration = classDuration;
+
+            this.tags = roomData.content2;
+
+            console.log('this tags before', this.tags);
+
+            this.newTagArray();
+            console.log('this tag after', this.tags);
           }
         })
         .catch(() => {
@@ -110,6 +98,10 @@ export default {
 
         });
     },
+
+    // newTagArray(startClass) {
+    //   this.tagsPosition = this.tags.map(x => this.calcCoordinates(this.tags[x].time, startClass));
+    // },
   },
 };
 </script>
