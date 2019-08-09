3b17750bcaa2be24a9da0e8211de0c74dcaddbd9
nicolas <nicolas.ducteil@omnilive.fr>
2019-08-09 18:14:38 +0200

    remove service workers

--- src/helpers/Auth.js
+++ src/helpers/Auth.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 // vérifie l'existence d'une session ouverte pour l'utilisateur
 export default function isAuthenticated() {
-  return axios.get('http://localhost:5000');
+  return axios.get(process.env.AXIOS_BASE_URL);
 }
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store/index';
-import './registerServiceWorker';
+// import './registerServiceWorker';
 
 
 Vue.prototype.moment = moment;
