fc3df2482c4b60850af1728cb759eff545fd47ef
nicolas <nicolas.ducteil@omnilive.fr>
2019-08-15 16:45:43 +0200

    suppression timeline link in header + change in data displayed in tags modal

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,28 +10,24 @@
       <v-btn flat to="/roomsList" v-if="isLoggedIn">
         <v-icon class="mr-2">account_box</v-icon>
         Classes
-        </v-btn>
+      </v-btn>
       <v-btn flat to="/home" v-if="isLoggedIn">
         <v-icon class="mr-2">home</v-icon>
         Home
-        </v-btn>
-      <v-btn flat to="/timeline" v-if="isLoggedIn">
-        <v-icon class="mr-2">timeline</v-icon>
-        Timeline
-        </v-btn>
+      </v-btn>
       <CreateRoom v-if="isLoggedIn && this.user.role === 'teacher'"/>
       <v-btn flat to="/register" v-if="!isLoggedIn">
         <v-icon class="mr-2">account_box</v-icon>
         Register
-        </v-btn>
+      </v-btn>
       <v-btn flat to="/login" v-if="!isLoggedIn">
         <v-icon class="mr-2">fingerprint</v-icon>
         Login
-        </v-btn>
+      </v-btn>
       <v-btn flat to="/login" v-if="isLoggedIn" @click="logout">
         <v-icon class="mr-2">exit_to_app</v-icon>
         Logout
-        </v-btn>
+      </v-btn>
     </v-toolbar-items>
   </v-toolbar>
 </template>
